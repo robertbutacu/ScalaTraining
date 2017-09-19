5ef2f2595569b106bed3cb954bb34a3c2990f7f7
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-19 21:48:27 +0300

    Added a nice fibo implementation.

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -3,13 +3,31 @@ import scala.math.sqrt
   * Created by Robert-PC on 9/19/2017.
   */
 object Arithmetic {
-  def isPrime(x: Int): Boolean = {
+  def isPrime1(x: Int): Boolean = {
     x match {
-      case _ if x < 2      => false
-      case 2               => true
+      case _ if x < 2 => false
+      case 2 => true
       case _ if x % 2 == 0 => false
-      case _               =>
+      case _ =>
         !(3 to sqrt(x.toDouble).floor.toInt by 2).toArray.exists(x % _ == 0)
     }
   }
+
+  /*
+    List(1,2,3,4,5,6)
+    List(2,3,4,5,6)
+
+    zipping the previous lists would give the result
+    List((1,2),(2,3),(3,4),(4,5),(5,6))
+
+    Being a stream, it goes like this:
+    0 and 1 are already found, followed by
+    (0,1).zip(1) => (0,1) => 1
+    0, 1, 1
+    (0, 1, 1).zip((1, 1)) => (0,1), (1,1) => 1, 2
+    and so on.
+   */
+  lazy val fibs: Stream[BigInt] = BigInt(0) #::
+    BigInt(1) #::
+    fibs.zip(fibs.tail).map { n => n._1 + n._2 }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,6 @@
   * Created by r.butacu on 7/19/2017.
   */
 object Main extends App {
-  println(Arithmetic.isPrime(5))
-  println(Arithmetic.isPrime(6))
-  println(Arithmetic.isPrime(15))
+  println(Arithmetic.fibs take 5 toList)
+
 }
