14cadb2922f45b5aa5bc274b1db74bbf110494b2
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-20 22:49:52 +0300

    Added Boolean functions.

---
+++ src/main/scala/LogicAndCodes.scala
@@ -0,0 +1,35 @@
+/**
+  * Created by Robert-PC on 9/20/2017.
+  */
+object LogicAndCodes {
+
+  def not(first: Boolean): Boolean = first match{
+    case true  => false
+    case false => true
+  }
+
+  def and(first: Boolean, second: Boolean): Boolean = {
+    (first, second) match {
+      case (true, true) => true
+      case _            => false
+    }
+  }
+
+  def or(first:Boolean, second: Boolean): Boolean = {
+    (first, second) match {
+      case (true, _)      => true
+      case (_, true)      => true
+      case (false, false) => false
+    }
+  }
+
+  def nand(first: Boolean, second: Boolean): Boolean = not(and(first, second))
+
+  def xor(first: Boolean, second: Boolean): Boolean = {
+    (first, second) match {
+      case (true, true)   => false
+      case (false, false) => false
+      case _              => true
+    }
+  }
+}
